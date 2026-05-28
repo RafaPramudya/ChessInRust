9b78b938f3b393179d2554938bc969be2f758f96
RafaPramudya <bedsetmodern@gmail.com>
2026-05-28 16:37:52 +0700

    Advanced FEN Parsing

--- src/chess.rs
+++ src/chess.rs
@@ -96,6 +96,49 @@ impl fmt::Display for Piece {
     }
 }
 
+#[derive(Debug)]
+pub enum Square {
+    Invalid,
+    Square(i8)
+}
+
+impl Square {
+    pub fn new(file: i8, rank: i8) -> Self {
+        let index = rank * 8 + file;
+        if index > 63 || index < 0  { Self::Invalid }
+        else                        { Self::Square(index) }
+    }
+
+    pub fn from(file: char, rank: i8) -> Self {
+        let file = file as i8 - 'a' as i8;
+
+        Self::new(file, rank)
+    }
+
+    pub fn from_notation(expr: &str) -> Self {
+        let mut expr = expr.chars();
+
+        let file = expr.next().unwrap() as i8 - 'a' as i8;
+        let rank = expr.next().unwrap() as i8 - '0' as i8;
+
+        Self::new(file, rank)
+    }
+
+    pub fn get_file(&self) -> Option<i8> {
+        match self {
+            Square::Invalid => None,
+            Square::Square(x) => Some(x % 8)
+        }
+    }
+
+    pub fn get_rank(&self) -> Option<i8> {
+        match self {
+            Square::Invalid => None,
+            Square::Square(x) => Some(x / 8)
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum Cell {
     Empty,
@@ -112,8 +155,42 @@ impl fmt::Display for Cell {
 }
 
 #[derive(Debug)]
+pub struct Castle {
+    white_king: bool,
+    white_queen: bool,
+    black_king: bool,
+    black_queen: bool,
+}
+
+impl Castle {
+    pub fn all_false() -> Self {
+        Self {
+            white_king: false,
+            white_queen: false,
+            black_king: false,
+            black_queen: false,
+        }
+    }
+
+    pub fn all_true() -> Self {
+        Self {
+            white_king: true,
+            white_queen: true,
+            black_king: true,
+            black_queen: true,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Board {
     cells: [Cell; 64],
+    white_to_play: bool,
+    castle_available: Castle,
+    enpassant: Square,
+
+    halfmove: u8,
+    fullmove: u8
 }
 
 impl fmt::Display for Board {
@@ -121,14 +198,22 @@ impl fmt::Display for Board {
         let mut output = String::new();
         output.reserve(256);
         
-        for i in 0..64 {
-            let cell = self.cells[i];
-            if i % 8 == 0 && i != 0 {
-                output.push('\n');
-            }
+        for rank in (0..8).rev() {
+            for file in 0..8 {
+                let index = rank * 8 + file;
+                let cell = self.cells[index];
 
-            write!(&mut output, "{} ", cell).unwrap();
+                write!(&mut output, "{} ", cell).unwrap();
+            }
+            output.push('\n');
         }
+
+        output.push('\n');
+        writeln!(&mut output, "WhiteToPlay: {}", self.white_to_play).unwrap();
+        writeln!(&mut output, "Caslte: {:?}", self.castle_available).unwrap();
+        writeln!(&mut output, "EnPassant: {:?}", self.enpassant).unwrap();
+        writeln!(&mut output, "HalfMove: {}", self.halfmove).unwrap();
+        writeln!(&mut output, "FullMove: {}", self.fullmove).unwrap();
 
         write!(f, "{}", output)
     }
@@ -137,36 +222,88 @@ impl fmt::Display for Board {
 impl Board {
     pub fn empty() -> Self {
         Self {
-            cells: [Cell::Empty; 64]
+            cells: [Cell::Empty; 64],
+            castle_available: Castle::all_false(),
+            white_to_play: true,
+            enpassant: Square::Invalid,
+            halfmove: 0,
+            fullmove: 0
         }
     }
 
-    pub fn new<T: AsRef<str>>(fen: T) -> Result<Self, Error> {
-        let fen = fen.as_ref();
+    fn parse_pieces(token: &str) -> Result<[Cell; 64], Error> {
         let mut cells = Vec::new();
-        cells.reserve(64);
+        // let token = token.chars();
+        let ranks = token.split('/').into_iter();
+        for rank in ranks.rev() {
+            let rank = rank.chars();
 
-        let fen = fen.chars();
-
-        for char in fen {
-            match char {
-                '1'..='8' => {
-                    let mut empty = vec![Cell::Empty; char as usize - '0' as usize];
-                    cells.append(&mut empty);
+            for char in rank {
+                match char {
+                    '1'..='8' => {
+                        let mut empty = vec![Cell::Empty; char as usize - '0' as usize];
+                        cells.append(&mut empty);
+                    }
+                    'p' | 'n' | 'b' | 'r' | 'q' | 'k' |
+                    'P' | 'N' | 'B' | 'R' | 'Q' | 'K'
+                    => {cells.push(Cell::Cell(Piece::new_from_char(char).unwrap()));}
+                    _ => {return Err(Error::InvalidFEN)}
                 }
-                '/' => {
-                    if cells.len() % 8 != 0 { return Err(Error::InvalidFEN) }
-                }
-                'p' | 'n' | 'b' | 'r' | 'q' | 'k' |
-                'P' | 'N' | 'B' | 'R' | 'Q' | 'K'
-                => {cells.push(Cell::Cell(Piece::new_from_char(char).unwrap()));}
-                ' ' => {}
-                _ => {}
             }
         }
 
-        Ok(Self {
-            cells: cells.try_into().unwrap()
+        // println!("{}", cells.len());
+        Ok(cells.try_into().expect("Cell dalam fen seharusnya berjumlah 64"))
+    }
+
+    fn parse_white_to_play(token: &str) -> Option<bool> {
+        match token {
+            "w" => Some(true),
+            "b" => Some(false),
+            _ => None
+        }
+    }
+
+    fn parse_castle(token: &str) -> Result<Castle, Error> {
+        let mut castle = Castle::all_false();
+
+        let token = token.chars();
+        for char in token {
+            match char {
+                'k' => castle.black_king = true,
+                'q' => castle.black_queen = true,
+                'K' => castle.white_king = true,
+                'Q' => castle.white_queen = true,
+                '-' => {}
+                _ => return Err(Error::InvalidFEN)
+            }
+        }
+
+        Ok(castle)
+    }
+
+    fn parse_enpassant(token: &str) -> Square {
+        if token == "-" { return Square::Invalid }
+        Square::from_notation(token)
+    }
+
+    pub fn new<T: AsRef<str>>(fen: T) -> Result<Self, Error> {
+        let mut fen = fen.as_ref().split_whitespace();
+
+        let cells = Self::parse_pieces(fen.next().unwrap())?;
+        let white_to_play = Self::parse_white_to_play(fen.next().unwrap()).unwrap();
+        let castle = Self::parse_castle(fen.next().unwrap())?;
+        let enpassant = Self::parse_enpassant(fen.next().unwrap());
+        let halfmove: u8 = fen.next().unwrap().trim().parse().unwrap();
+        let fullmove: u8 = fen.next().unwrap().trim().parse().unwrap();
+
+        Ok(Self{
+            cells,
+            white_to_play,
+            castle_available: castle,
+            enpassant,
+            halfmove,
+            fullmove
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod chess;
 
 fn main() -> Result<(), chess::Error> {
-    let board = chess::Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")?;
+    let board = chess::Board::new("8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1")?;
     println!("{}", board);
 
     Ok(())
